3f299f2e73c1813b2dde9c36621995084784005b
Timiz Depot <timizdepot@gmail.com>
2022-12-30 23:59:49 -0500

    updated the node-exporter installation script

--- install-node-exporter.sh
+++ install-node-exporter.sh
@@ -1,3 +1,18 @@
+# sudo useradd --no-create-home node_exporter
+
+# wget https://github.com/prometheus/node_exporter/releases/download/v1.0.1/node_exporter-1.0.1.linux-amd64.tar.gz
+# tar xzf node_exporter-1.0.1.linux-amd64.tar.gz
+# sudo cp node_exporter-1.0.1.linux-amd64/node_exporter /usr/local/bin/node_exporter
+# rm -rf node_exporter-1.0.1.linux-amd64.tar.gz node_exporter-1.0.1.linux-amd64
+
+# sudo cp node-exporter.service /etc/systemd/system/node-exporter.service
+
+# sudo systemctl daemon-reload
+# sudo systemctl enable node-exporter
+# sudo systemctl start node-exporter
+# sudo systemctl status node-exporter
+
+# node-exporter installations
 sudo useradd --no-create-home node_exporter
 
 wget https://github.com/prometheus/node_exporter/releases/download/v1.0.1/node_exporter-1.0.1.linux-amd64.tar.gz
@@ -5,7 +20,9 @@ tar xzf node_exporter-1.0.1.linux-amd64.tar.gz
 sudo cp node_exporter-1.0.1.linux-amd64/node_exporter /usr/local/bin/node_exporter
 rm -rf node_exporter-1.0.1.linux-amd64.tar.gz node_exporter-1.0.1.linux-amd64
 
-sudo cp node-exporter.service /etc/systemd/system/node-exporter.service
+# setup the node-exporter dependencies
+sudo git clone -b installations https://github.com/timizdepot/devops-fully-automated.git /tmp/devops-fully-automated
+sudo cp /tmp/devops-fully-automated/prometheus-setup-dependencies/node-exporter.service /etc/systemd/system/node-exporter.service
 
 sudo systemctl daemon-reload
 sudo systemctl enable node-exporter
